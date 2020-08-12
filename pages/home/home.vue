33074db8e1a6b0db11507c35df3ade71902543f1
shine-ztf <15180284387@163.com>
2020-08-12 19:42:45 +0800

    feat: fix

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,39 +1,74 @@
 <template>
 	<view class="u-page">
 		<u-navbar ref="navbar" :is-back="false" :title="vuex_tabbar[0].text" :title-color="'#ffffff'" :background="{backgroundColor:'#00aaff'}"></u-navbar>
+		<u-tabs ref="tabs" :list="list" :is-scroll="false" :current="current" @change="tabsChange" :bg-color="'#ffffff'"></u-tabs>
 		<view class="content">
-			<u-tabs ref="tabs" :list="list" :is-scroll="false" :current="current" @change="tabsChange" :bg-color="'#ffffff'"></u-tabs>
 			<scroll-view scroll-y :style="[{height:`calc(100vh - (${statusBarHeight + navbarHeight + tabbarHeight}px) - ${tabsHeight}rpx)`}]">
-				<view class="u-card-wrap">
-					<u-card :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
-						<view class="" slot="body">
-							<view class="u-body-item u-flex u-row-between u-p-b-0">
-								<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
-								<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
-							</view>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
 						</view>
-						<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
-					</u-card>
-					<u-card :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
-						<view class="" slot="body">
-							<view class="u-body-item u-flex u-row-between u-p-b-0">
-								<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
-								<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
-							</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
 						</view>
-						<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
-					</u-card>
-					<u-card :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
-						<view class="" slot="body">
-							<view class="u-body-item u-flex u-row-between u-p-b-0">
-								<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
-								<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
-							</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
 						</view>
-						<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
-					</u-card>
-				</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
+						</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
+						</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
+						</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
+				<u-card :margin="'30rpx 20rpx'" :title="title" :sub-title="subTitle" :thumb="thumb" :border-radius="0">
+					<view class="" slot="body">
+						<view class="u-body-item u-flex u-row-between u-p-b-0">
+							<view class="u-body-item-title u-line-2">釉色渲染仕女图韵味被私藏，而你嫣然的一笑如含苞待放</view>
+							<image src="https://img12.360buyimg.com/n7/jfs/t1/102191/19/9072/330688/5e0af7cfE17698872/c91c00d713bf729a.jpg" mode="aspectFill"></image>
+						</view>
+					</view>
+					<view class="" slot="foot"><u-icon name="chat-fill" size="34" color="" label="30评论"></u-icon></view>
+				</u-card>
 			</scroll-view>
+			<u-back-top :scroll-top="scrollTop" top="600"></u-back-top>
 		</view>
 		<u-tabbar ref="tabbar" :list="vuex_tabbar" :active-color="'#ff557f'"></u-tabbar>
 	</view>
@@ -47,6 +82,7 @@
 				tabbarHeight: 0,
 				navbarHeight: 0,
 				statusBarHeight: 0,
+				scrollTop: 0,
 				title: '素胚勾勒出青花，笔锋浓转淡',
 				subTitle: '2020-05-15',
 				thumb: 'http://pic2.sc.chinaz.com/Files/pic/pic9/202002/hpic2119_s.jpg',
@@ -87,17 +123,7 @@
 	.content {
 		background-color: #f7f7f7;
 	}
-	
-	.content .tabs-bar {
-		position: fixed;
-		top: 50;
-	}
-	
-	.u-card-wrap { 
-		background-color: $u-bg-color;
-		padding: 1px;
-	}
-		
+
 	.u-body-item {
 		font-size: 32rpx;
 		color: #333;
